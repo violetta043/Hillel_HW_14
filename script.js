c7b41dc910227208c581c9adbbc0139c862dd91b
Violetta286 <sinitsina.viol@gmail.com>
2024-04-10 16:19:16 +0300

    second commit

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const infoForm = document.querySelector('.user_info');
 const submitBtn = document.querySelector('.btn_confirm');
 const formUser = document.querySelector('.user_info');
 const outputDiv = document.querySelector('.output');
+const pib = document.querySelector('.pib');
+const phone = document.querySelector('.phone');
+const email = document.querySelector('.email');
+
+const regexPib = /[A-Z][a-z]{1,} [A-Z][a-z]{1,} [A-Z][a-z]{1,}/;
+const regexPhone = /^\+?[0-9()-]{10,}$/;
+const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 
 
 
@@ -21,7 +29,6 @@ formUser.addEventListener('submit', function(event){
         formObj[key] = value;
     })
 
-    validate(formObj);
 
     if(validate(formObj)) {
         let outputStr = "";
@@ -37,11 +44,11 @@ formUser.addEventListener('submit', function(event){
 
 function validate(object){
     let isValid = true;
-    if(object.pib.length < 4){
-        document.querySelector('.valid_fn').style.display = 'block';
+    if(!object.pib){
+        document.querySelector('.valid_fn').style.display = 'block'; 
         isValid = false; 
-    }else{
-        document.querySelector('.valid_fn').style.display = 'none';
+    } else {
+        document.querySelector('.valid_fn').style.display = 'none'; 
     }
     if(!object.select_city){
         document.querySelector('.valid_s').style.display = 'block'; 
@@ -69,3 +76,26 @@ function validate(object){
     }
     return isValid;
 }
+
+function validateField(event, regex, errorClass){
+    const isValid = regex.test(event.target.value);
+
+    if(isValid){
+        document.querySelector(errorClass).style.display = 'none';
+    } else {
+        document.querySelector(errorClass).style.display = 'block';
+    }
+}
+
+pib.addEventListener('input', (event) => {
+  validateField(event, regexPib, '.valid_fn');
+});
+
+phone.addEventListener('input', (event) => {
+  validateField(event, regexPhone, '.valid_phone');
+})
+
+email.addEventListener('input', (event)=> {
+  validateField(event, regexEmail, '.valid_email');
+})
+
